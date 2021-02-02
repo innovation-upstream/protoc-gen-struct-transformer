977dd902baa0a3bee3dcd59c5a0bfbd7409f237b
Zach Lowden <zach@mobl.ca>
2021-02-02 00:07:14 +0000

    wip

--- generator/field.go
+++ generator/field.go
@@ -192,7 +192,7 @@ func processSimpleField(w io.Writer, pname, gname string, ftype *descriptor.Fiel
 	case sft != tgo:
 		p := t.pbType
 		if *ftype == descriptor.FieldDescriptorProto_TYPE_ENUM {
-			p = f.ProtoType + "Enum"
+			p = sf.Type + "Enum"
 		}
 		if p == "" {
 			p = t.goType
